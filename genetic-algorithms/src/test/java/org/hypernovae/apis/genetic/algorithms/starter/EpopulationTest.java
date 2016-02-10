599d2047112f84da0a9fb47ca29e7c40a97ff07f
alainlompo <alain.lompo@gmail.com>
2016-02-10 23:30:16 +0100

    Enriching the tests

--- genetic-algorithms/src/test/java/org/hypernovae/apis/genetic/algorithms/starter/EpopulationTest.java
+++ genetic-algorithms/src/test/java/org/hypernovae/apis/genetic/algorithms/starter/EpopulationTest.java
@@ -1,6 +1,8 @@
 package org.hypernovae.apis.genetic.algorithms.starter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -11,5 +13,35 @@ public class EpopulationTest {
 		EPopulation ePop = new EPopulation(150);
 		assertEquals(150, ePop.size());
 	}
+	
+	@Test
+	public void construction_with_one_param_is_ok_Test() {
+		EPopulation ePop = new EPopulation(5);
+		Android[] androids = ePop.getAndroids();
+		assertEquals(5, androids.length);
+		for (int i = 0; i < androids.length; i++) {
+			assertNull(androids[i]);
+		}
+	}
+	
+	@Test(expected= NegativeArraySizeException.class)
+	public void construction_with_one_param_is_not_ok_case_1_Test() {
+		EPopulation ePop = new EPopulation(-5);
+		assertEquals(-5, ePop.getAndroids().length);
+	}
+	
+	@Test
+	public void construction_with_two_params_is_ok_Test() {
+		EPopulation ePop = new EPopulation(5,3);
+		Android[] androids = ePop.getAndroids();
+		assertEquals(5, androids.length);
+		for (int i = 0; i < androids.length; i++) {
+			assertNotNull(androids[i]);
+			assertEquals(3, androids[i].getEChromosomeLength());
+			
+		}
+	}
+	
+	
 	
 }
